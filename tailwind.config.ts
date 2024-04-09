f4c28668f175aa53639a2cadb671c7237914aee4
Guilherme Silva <2003silvagui@gmail.com>
2024-04-09 19:20:32 -0300

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#003C43",
+        secondary: "#135D66",
+        light: "#77B0AA",
+        "max-light": "#E3FEF7"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
