e5f32c42847841f784695cbd8f55cab485e1c592
Guilherme Silva <2003silvagui@gmail.com>
2024-04-12 17:25:24 -0300

    feat: spline component

---
+++ src/app/components/spline/spline.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect } from "react";
+import { Application } from "@splinetool/runtime";
+
+const Spline = () => {
+  useEffect(() => {
+    const loadSpline = async () => {
+      const canvas: any = document.getElementById("canvas3d");
+      const app = new Application(canvas);
+      await app.load(
+        "https://prod.spline.design/k4C28pPchcJ9QxyT/scene.splinecode"
+      );
+    };
+
+    loadSpline();
+    return () => {};
+  }, []);
+
+  return (
+    <div className="absolute z-0 w-screen h-screen">
+      <canvas id="canvas3d" />
+    </div>
+  );
+};
+
+export default Spline;
